4fb26ca556c9ec7838c6386a5e66c4f047b91106
Lucas Brunini <brunini.lucas94@gmail.com>
2024-05-08 00:12:28 -0300

    Fix reaction list allocation order

--- motbinImport.py
+++ motbinImport.py
@@ -749,19 +749,17 @@ class MotbinStruct:
                 self.writeInt(self.getPushbackFromId(pushback), 8)
 
             # self.skip(0x8) unused
-            self.writeInt(reaction_list['vertical_pushback'], 4)
             self.writeInt(reaction_list['front_direction'], 2)
             self.writeInt(reaction_list['back_direction'], 2)
             self.writeInt(reaction_list['left_side_direction'], 2)
             self.writeInt(reaction_list['right_side_direction'], 2)
             self.writeInt(reaction_list['front_counterhit_direction'], 2)
-            self.writeInt(reaction_list['downed_direction'], 2)
             self.writeInt(reaction_list['front_rotation'], 2)
             self.writeInt(reaction_list['back_rotation'], 2)
             self.writeInt(reaction_list['left_side_rotation'], 2)
             self.writeInt(reaction_list['right_side_rotation'], 2)
             self.writeInt(reaction_list['front_counterhit_rotation'], 2)
-            self.writeInt(reaction_list['downed_rotation'], 2)
+            self.writeInt(reaction_list['vertical_pushback'], 4)
             self.writeInt(reaction_list['standing'], 2)
             self.writeInt(reaction_list['crouch'], 2)
             self.writeInt(reaction_list['ch'], 2)
@@ -776,6 +774,8 @@ class MotbinStruct:
             self.writeInt(reaction_list['crouch_block'], 2)
             self.writeInt(reaction_list['wallslump'], 2)
             self.writeInt(reaction_list['downed'], 2)
+            self.writeInt(reaction_list['downed_direction'], 2)
+            self.writeInt(reaction_list['downed_rotation'], 2)
             # self.skip(4) unused
 
         return self.reaction_list_ptr, len(self.m['reaction_list'])
